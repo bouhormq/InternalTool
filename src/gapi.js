0f44688499aaeecf96ff09b27ddfa7aa724621e8
bouhormq <bouhormq@gmail>
2023-01-29 14:46:43 +0000

    riders

--- src/gapi.js
+++ src/gapi.js
@@ -1,9 +1,9 @@
 import { gapi } from 'gapi-script'
+import { SCOPE, CLIENT_ID, API_KEY, DISCOVERY_DOCS } from './gapiVar';
 
 
 export var GoogleAuth;
 export function onLoad(){
-  var SCOPE = 'openid https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/calendar';
   function handleClientLoad() {
     // Load the API's client and auth2 modules.
     // Call the initClient function after the modules load.
@@ -12,16 +12,15 @@ export function onLoad(){
 
 function initClient() {
   // In practice, your app can retrieve one or more discovery documents.
-  var discoveryUrl = 'https://www.googleapis.com/discovery/v1/apis/drive/v3/rest';
 
   // Initialize the gapi.client object, which app uses to make API requests.
   // Get API key and client ID from API Console.
   // 'scope' field specifies space-delimited list of access scopes.
     gapi.client.init({
-        apiKey: "AIzaSyBi-kWJ091WgznkX5WSfJZGo50Kmkfkp20",
-        clientId: "414345920391-du9k4dacovq09q49q6dgqsqnp3vdf4fm.apps.googleusercontent.com",
+        apiKey: API_KEY,
+        clientId: CLIENT_ID,
         scope: SCOPE,
-      'discoveryDocs': [discoveryUrl],
+      'discoveryDocs': DISCOVERY_DOCS,
     }).then(async function () {
       console.log(gapi.auth2.getAuthInstance())
       GoogleAuth = gapi.auth2.getAuthInstance()
@@ -30,3 +29,87 @@ function initClient() {
   handleClientLoad()
 }
 
+//export a function that gets start time(date picker), location, name 
+export const addCalendarEvent = (startDate,address,clientName) => {
+  
+  gapi.load('client:auth2', () => {
+    gapi.client.init({
+      apiKey: API_KEY,
+      clientId: CLIENT_ID,
+      scope: SCOPE,
+      'discoveryDocs': DISCOVERY_DOCS,
+    })
+
+
+    gapi.client.load('calendar', 'v3')
+//time zone list:
+// https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
+    let timeZone = "Asia/Jerusalem"; 
+    let duration = '00:30:00'; //duration of each event, here 30 minuts
+
+
+  //event start time - im passing datepicker time, and making it match      //with the duration time, you can just put iso strings:
+//2020-06-28T09:00:00-07:00' 
+
+    let msDuration = (Number(duration.split(':')[0]) * 60 * 60 + Number(duration.split(':')[1]) * 60  + Number(duration.split(':')[2])) * 1000;
+    let endDate = new Date(startDate.getTime() + msDuration);
+    let isoStartDate = new Date(startDate.getTime()-new Date().getTimezoneOffset()*60*1000).toISOString().split(".")[0];
+    let isoEndDate = new Date(endDate.getTime()-(new Date().getTimezoneOffset())*60*1000).toISOString().split(".")[0];
+
+
+//sign in with pop up window
+    gapi.auth2.getAuthInstance().signIn()
+    .then(() => { 
+      let event = {
+        'summary': clientName, // or event name
+        'location': address, //where it would happen
+        'start': {
+          'dateTime': isoStartDate,
+          'timeZone': timeZone
+        },
+        'end': {
+          'dateTime': isoEndDate,
+          'timeZone': timeZone
+        },
+        'recurrence': [
+          'RRULE:FREQ=DAILY;COUNT=1' 
+        ],
+        'reminders': {
+          'useDefault': false,
+          'overrides': [
+            {'method': 'popup', 'minutes': 20}
+          ]
+        }
+      }
+       
+//if you need to list your events than keep it
+       gapi.client.calendar.events.list({
+        'calendarId': 'primary',
+        'timeMin': (new Date()).toISOString(),
+        'showDeleted': false,
+        'singleEvents': true,
+        'maxResults': 10,
+        'orderBy': 'startTime'
+      }).then(response => {
+        const events = response.result.items
+        console.log('EVENTS: ', events)
+      })
+      
+//end of event listing
+     
+      let request = gapi.client.calendar.events.insert({
+        'calendarId': 'primary',
+        'resource': event,
+      })
+
+
+      request.execute(event => {
+        console.log(event)
+        window.open(event.htmlLink)
+      })
+      
+
+
+    })
+  })
+}
